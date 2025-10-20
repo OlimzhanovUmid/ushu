b350ed53db6c96571e5e33785092aaf1d5ace052
Umid Olimzhanov <uolimzhanov@gmail.com>
2025-10-20 18:12:15 +0500

    fix admin panel url

--- ushu/urls.py
+++ ushu/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import include, url, static
 from django.contrib import admin
 
 urlpatterns = [
-                  url(r'^admin/', include(admin.site.urls)),
+                  url(r'^admin/', admin.site.urls),
                   url(r'^', include('tablo.urls')),
               ] + static.static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) \
               + static.static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
